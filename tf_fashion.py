30108446f9e165756016a5237fc643936260f4b2
jpramassini <jpramassini@gmail.com>
2019-05-11 10:04:51 -0400

    Model now trains and evaluates. ~87% test accuracy.

--- tf_fashion.py
+++ tf_fashion.py
@@ -19,12 +19,28 @@ train_images = train_images / 255.0
 test_images = test_images / 255.0
 
 #display first 25 images in dataset with labels.
-plt.figure(figsize=(10,10))
-for i in range(25):
-    plt.subplot(5,5, i+1)
-    plt.xticks([])
-    plt.yticks([])
-    plt.grid(False)
-    plt.imshow(train_images[i], cmap=plt.cm.binary)
-    plt.xlabel(class_names[train_labels[i]])
-plt.show()
+# plt.figure(figsize=(10,10))
+# for i in range(25):
+#     plt.subplot(5,5, i+1)
+#     plt.xticks([])
+#     plt.yticks([])
+#     plt.grid(False)
+#     plt.imshow(train_images[i], cmap=plt.cm.binary)
+#     plt.xlabel(class_names[train_labels[i]])
+# plt.show()
+
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28, 28)),
+    keras.layers.Dense(128, activation='relu'),
+    keras.layers.Dense(10, activation='softmax')
+])
+
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy',
+                metrics=['accuracy'])
+
+#training model on training data.
+history = model.fit(train_images, train_labels, epochs=5)
+
+#evaluating trained model
+print("Evaluating trained model...\n")
+test_loss, test_acc = model.evaluate(test_images, test_labels)
